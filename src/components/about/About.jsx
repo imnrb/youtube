c1c6ee2fe2708082279632551bae280d32cce695
imnrb <67147637+imnrb@users.noreply.github.com>
2022-02-27 10:34:44 +0530

    responsiveness of intro component

---
+++ src/components/about/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./about.css"
+const About
+    = () => {
+        return (
+            <div className='a'>About
+                <div className='a-left'>
+                    <div className="a-card bg"></div>
+                    <div className="a-card">
+                        <img src="" alt="" className='a-img'/>
+                    </div>
+
+                </div>
+                <div className='a-right'>right</div>
+            </div>
+        )
+    }
+
+export default About
